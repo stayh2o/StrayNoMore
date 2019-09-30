25c1f3cd3e4419ca7de03111d00c886b779710da
Kyle Lobo <kylelobo11898@gmail.com>
2019-09-30 12:32:27 +0530

    Hopeful fix for PayLoadTooLargeError

--- Server/API/server.js
+++ Server/API/server.js
@@ -25,9 +25,9 @@ mongoose.connect('mongodb://localhost/straynomore');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+// app.use(bodyParser.json());
+app.use(bodyParser.json({limit: "50mb", type:'application/json'}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000, type:'application/x-www-form-urlencoding'}));
 
 app.get('/', (req, res) => {
     //console.log(limit)
